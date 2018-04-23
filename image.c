889bd95101f5332eae74bbadc95d51d7e38d9660
Heng Li <lh3@me.com>
2018-04-23 14:02:08 -0400

    semi-symmetric PNG

--- image.c
+++ image.c
@@ -17,7 +17,7 @@ void hk_pair_image(const struct hk_sdict *d, int32_t n_pairs, const struct hk_pa
 {
 	int64_t tot_len, *off;
 	int32_t i, j, n1, *tmp, ww = w * w, c_max, pixel_bp;
-	uint8_t *p, *buf;
+	uint8_t *buf;
 	double s, t;
 	struct cnt_aux *cnt;
 
@@ -65,12 +65,14 @@ void hk_pair_image(const struct hk_sdict *d, int32_t n_pairs, const struct hk_pa
 
 	t = 255.0 / c_max;
 	buf = CALLOC(uint8_t, ww * 3);
-	for (i = 0, p = buf; i < w; ++i) {
-		for (j = 0; j < w; ++j) {
+	for (i = 0; i < w; ++i) {
+		for (j = i; j < w; ++j) {
 			int32_t z = i * w + j;
+			uint8_t *q = &buf[(j * w + i) * 3];
+			uint8_t *p = &buf[(i * w + j) * 3];
 			struct cnt_aux *c = &cnt[z];
 			if (c->tot == 0) {
-				*p++ = 0, *p++ = 0, *p++ = 0;
+				*p++ = 0, *p++ = 0, *p++ = 0, *q++ = 0, *q++ = 0, *q++ = 0;
 			} else {
 				int x, k, max_k = -1, max = -1;
 				for (k = 0; k < 4; ++k)
@@ -79,11 +81,11 @@ void hk_pair_image(const struct hk_sdict *d, int32_t n_pairs, const struct hk_pa
 				if (max == 0 || max < c->tot/2) x = (int)(c->tot * t);
 				else x = (int)(max * t);
 				if (x > 255) x = 255;
-				if (max == 0 || max < c->tot/2) *p++ = x/2, *p++ = x/2, *p++ = x/2;
-				else if (max_k == 0) *p++ = x, *p++ = 0, *p++ = 0;
-				else if (max_k == 1) *p++ = x, *p++ = x, *p++ = 0;
-				else if (max_k == 2) *p++ = 0, *p++ = x, *p++ = 0;
-				else if (max_k == 3) *p++ = 0, *p++ = x, *p++ = x;
+				if (max == 0 || max < c->tot/2) *p++ = x/2, *p++ = x/2, *p++ = x/2, *q++ = x/2, *q++ = x/2, *q++ = x/2;
+				else if (max_k == 0) *p++ = x, *p++ = 0, *p++ = 0, *q++ = x, *q++ = 0, *q++ = 0;
+				else if (max_k == 1) *p++ = x, *p++ = 0, *p++ = x, *q++ = 0, *q++ = x, *q++ = x;
+				else if (max_k == 2) *p++ = 0, *p++ = x, *p++ = x, *q++ = x, *q++ = 0, *q++ = x;
+				else if (max_k == 3) *p++ = 0, *p++ = x, *p++ = 0, *q++ = 0, *q++ = x, *q++ = 0;
 			}
 		}
 	}
@@ -91,6 +93,7 @@ void hk_pair_image(const struct hk_sdict *d, int32_t n_pairs, const struct hk_pa
 	// write lines
 	for (i = 0; i < d->n - 1; ++i) {
 		int x = (int)(off[i+1] * s + .499);
+		uint8_t *p;
 		for (j = 0, p = &buf[x * w * 3]; j < w; ++j, p += 3)
 			p[0] = 32, p[1] = 32, p[2] = 32;
 		for (j = 0, p = &buf[x * 3]; j < w; ++j, p += w * 3)
