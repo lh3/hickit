c684832fb91023a4ce4c645624b4f4b99f5d44a1
Heng Li <lh3@me.com>
2018-05-16 15:32:34 -0400

    expose -p to fdg-multi.pl

--- fdg-multi.pl
+++ fdg-multi.pl
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 use Getopt::Std;
 
-my %opts = (k=>0.125, r=>2.0);
-getopts('k:r:', \%opts);
-die "Usage: fdg-multi.pl [-k repCoef] [-r refRadius] <in.pairs>\n" if @ARGV == 0;
+my %opts = (k=>0.125, r=>2.0, p=>0.51);
+getopts('k:r:p:', \%opts);
+die "Usage: fdg-multi.pl [-k repCoef] [-r refRadius] [-p probThres] <in.pairs>\n" if @ARGV == 0;
 
 my $conf = [["2m", 2000, 0.01], ["250k", 1000, 0.02], ["50k", 500, 0.03], ["20k", 500, 0.04]];
 
@@ -19,11 +19,11 @@ $prefix =~ s/\.pairs(\.gz)?$//;
 
 my ($prev, $next);
 $next = "$prefix.$conf->[0][0].3dg.gz";
-print "$hickit bin -g -k $opts{k} -r $opts{r} -b $conf->[0][0] -n $conf->[0][1] -e $conf->[0][2] $in 2> $prefix.3dg.log | gzip > $next\n";
+print "$hickit bin -g -p $opts{p} -k $opts{k} -r $opts{r} -b $conf->[0][0] -n $conf->[0][1] -e $conf->[0][2] $in 2> $prefix.3dg.log | gzip > $next\n";
 $prev = $next;
 for (my $i = 1; $i < @$conf; ++$i) {
 	$next = $i == @$conf - 1? "$prefix.3dg.gz" : "$prefix.$conf->[$i][0].3dg.gz";
-	print "$hickit bin -g -k $opts{k} -r $opts{r} -i $prev -b $conf->[$i][0] -n $conf->[$i][1] -e $conf->[$i][2] $in 2>> $prefix.3dg.log | gzip > $next\n";
+	print "$hickit bin -g -p $opts{p} -k $opts{k} -r $opts{r} -i $prev -b $conf->[$i][0] -n $conf->[$i][1] -e $conf->[$i][2] $in 2>> $prefix.3dg.log | gzip > $next\n";
 	$prev = $next;
 }
 
