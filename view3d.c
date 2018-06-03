7a356879e941f2c2c027dd40ba82a6beb97137e7
Heng Li <lh3@me.com>
2018-06-03 12:10:27 -0400

    optionally show balls

--- view3d.c
+++ view3d.c
@@ -18,6 +18,7 @@ struct {
 	int line_width;
 	int n_hl;
 	int is_white;
+	int use_ball;
 	char **hl;
 	float feat_lo, feat_hi;
 	int feat_color;
@@ -35,6 +36,19 @@ static inline void set_feat_color(float x, float alpha)
 
 static void cb_draw(void)
 {
+	static const double X = .525731112119133606, Z = .850650808352039932;
+	static GLfloat vdata[12][3] = {
+		{-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+		{0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+		{Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+	};
+	static GLuint tindices[20][3] = {
+		{1,4,0}, {4,9,0}, {4,5,9}, {8,5,4}, {1,8,4},
+		{1,10,8}, {10,3,8}, {8,3,5}, {3,2,5}, {3,7,2},
+		{3,10,7}, {10,6,7}, {6,11,7}, {6,0,11}, {6,1,0},
+		{10,1,6}, {11,0,9}, {2,11,9}, {5,2,9}, {11,2,7}
+	};
+
 	struct hk_bmap *m = global.m;
 	int32_t i;
 
@@ -54,6 +68,27 @@ static void cb_draw(void)
 			glVertex3f(m->x[off+j][0], m->x[off+j][1], m->x[off+j][2]);
 		}
 		glEnd();
+		if (global.use_ball) {
+			for (j = 0; j < cnt; ++j) {
+				int k;
+				if (m->feat && global.feat_color) set_feat_color(m->feat[off+j], global.alpha[i]);
+				glPushMatrix();
+				glTranslatef(m->x[off+j][0], m->x[off+j][1], m->x[off+j][2]);
+				glScalef(0.01, 0.01, 0.01);
+#if 1
+				glBegin(GL_TRIANGLES);
+				for (k = 0; k < 20; ++k) {
+					glVertex3fv(&vdata[tindices[k][0]][0]);
+					glVertex3fv(&vdata[tindices[k][1]][0]);
+					glVertex3fv(&vdata[tindices[k][2]][0]);
+				}
+				glEnd();
+#else
+				glutSolidSphere(1, 5, 4);
+#endif
+				glPopMatrix();
+			}
+		}
 	}
 	for (i = 0; i < m->d->n; ++i) {
 		int32_t j, cnt = (int32_t)m->offcnt[i];
@@ -118,6 +153,9 @@ static void cb_key(unsigned char key, int x, int y)
 		rotate(2, 1.0f);
 	} else if (key == ']') {
 		rotate(2, -1.0f);
+	} else if (key == 'b' || key == 'B') {
+		global.use_ball = !global.use_ball;
+		cb_draw();
 	} else if (key == 'z' || key == 'Z') {
 		global.is_white = !global.is_white;
 		cb_draw();
